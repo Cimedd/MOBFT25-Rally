28b97d3a19a1d86d9561661e98d9722687bdda5c
victorc2712 <victorchristian2712@gmail.com>
2025-05-19 17:17:09 +0700

    Fix Bug Message Box
    Perbaikan:
    - Kode kartu 12 tidak bisa diinput
    - Message Box tidak menampilkan kartu tsb Zonk atau Relic

--- app/Http/Controllers/RelicController.php
+++ app/Http/Controllers/RelicController.php
@@ -19,8 +19,12 @@ class RelicController extends Controller
             1 => [
                 'code' => 'POHON',
                 'question' => 'Apa kepanjangan dari UBAYA?',
-                'options' => ['A. Universitas Brawijaya', 'B. Universitas Negeri Surabaya', 
-                                'C. Universitas Surabaya', 'D. Universitas Banyuwangi'],
+                'options' => [
+                    'A. Universitas Brawijaya',
+                    'B. Universitas Negeri Surabaya',
+                    'C. Universitas Surabaya',
+                    'D. Universitas Banyuwangi'
+                ],
                 'answer' => 'C. Universitas Surabaya',
                 'is_relic' => true,
                 'answered' => false,
@@ -29,8 +33,12 @@ class RelicController extends Controller
             2 => [
                 'code' => 'RIMBA',
                 'question' => 'Pulau Sulawesi terletak di antara dua pulau besar, yaitu ...',
-                'options' => ['A. Jawa dan Sumatra', 'B. Kalimantan dan Papua', 
-                                'C. Bali dan Lombok', 'D. Nusa Tenggara dan Maluku'],
+                'options' => [
+                    'A. Jawa dan Sumatra',
+                    'B. Kalimantan dan Papua',
+                    'C. Bali dan Lombok',
+                    'D. Nusa Tenggara dan Maluku'
+                ],
                 'answer' => 'B. Kalimantan dan Papua',
                 'is_relic' => false,
                 'answered' => false,
@@ -39,8 +47,12 @@ class RelicController extends Controller
             3 => [
                 'code' => 'LIAR',
                 'question' => 'Apa nama kampus UBAYA yang berada di pegunungan?',
-                'options' => ['A. Ubaya Ngagel', 'B. Ubaya Tenggilis', 
-                                'C. Ubaya Training Center', 'D. Kampus Fakultas Kedokteran'],
+                'options' => [
+                    'A. Ubaya Ngagel',
+                    'B. Ubaya Tenggilis',
+                    'C. Ubaya Training Center',
+                    'D. Kampus Fakultas Kedokteran'
+                ],
                 'answer' => 'C. Ubaya Training Center',
                 'is_relic' => true,
                 'answered' => false,
@@ -49,8 +61,12 @@ class RelicController extends Controller
             4 => [
                 'code' => 'ALAM',
                 'question' => 'Rumah adat khas suku Toraja dikenal dengan nama ...',
-                'options' => ['A. Rumah Gadang', 'B. Tongkonan', 
-                                'C. Honai', 'D. Lamin'],
+                'options' => [
+                    'A. Rumah Gadang',
+                    'B. Tongkonan',
+                    'C. Honai',
+                    'D. Lamin'
+                ],
                 'answer' => 'B. Tongkonan',
                 'is_relic' => true,
                 'answered' => false,
@@ -77,8 +93,12 @@ class RelicController extends Controller
             7 => [
                 'code' => 'KABUT',
                 'question' => 'Fakultas apa yang tidak ada di Universitas Surabaya?',
-                'options' => ['A. Fakultas Hukum', 'B. Fakultas Kedoketeran', 
-                                'C. Fakultas Psikologi', 'D. Fakultas Ilmu Sosial dan Politik'],
+                'options' => [
+                    'A. Fakultas Hukum',
+                    'B. Fakultas Kedoketeran',
+                    'C. Fakultas Psikologi',
+                    'D. Fakultas Ilmu Sosial dan Politik'
+                ],
                 'answer' => 'D. Fakultas Ilmu Sosial dan Politik',
                 'is_relic' => false,
                 'answered' => false,
@@ -114,8 +134,12 @@ class RelicController extends Controller
             11 => [
                 'code' => 'SUNYI',
                 'question' => 'Dimanakah mahasiswa teknik Elektro bisa mengurus administrasi di jurusannya?',
-                'options' => ['A. PAJ Teknik Informatika', 'B. PAJ Teknik Elektro', 
-                                'C. TU Fakultas Teknik', 'D. PAJ Teknik Industri'],
+                'options' => [
+                    'A. PAJ Teknik Informatika',
+                    'B. PAJ Teknik Elektro',
+                    'C. TU Fakultas Teknik',
+                    'D. PAJ Teknik Industri'
+                ],
                 'answer' => 'B. PAJ Teknik Elektro',
                 'is_relic' => true,
                 'answered' => false,
@@ -124,8 +148,12 @@ class RelicController extends Controller
             12 => [
                 'code' => 'ANGIN',
                 'question' => 'Tana Toraja terkenal dengan tradisi menyimpan jenazah di ...',
-                'options' => ['A. Dalam tanah', 'B. Dalam rumah adat', 
-                                'C. Gua batu atau tebing', 'D. Sungai'],
+                'options' => [
+                    'A. Dalam tanah',
+                    'B. Dalam rumah adat',
+                    'C. Gua batu atau tebing',
+                    'D. Sungai'
+                ],
                 'answer' => 'C. Gua batu atau tebing',
                 'is_relic' => true,
                 'answered' => false,
@@ -136,8 +164,12 @@ class RelicController extends Controller
             1 => [
                 'code' => 'GUA',
                 'question' => 'Dimanakah lokasi kampus utama Universitas Surabaya?',
-                'options' => ['A. Surabaya Barat', 'B. Surabaya Timur', 
-                                'C. Trawas, Mojokerto', 'D. Tenggilis, Surabaya'],
+                'options' => [
+                    'A. Surabaya Barat',
+                    'B. Surabaya Timur',
+                    'C. Trawas, Mojokerto',
+                    'D. Tenggilis, Surabaya'
+                ],
                 'answer' => 'D. Tenggilis, Surabaya',
                 'is_relic' => true,
                 'answered' => false,
@@ -155,8 +187,12 @@ class RelicController extends Controller
             3 => [
                 'code' => 'SEMAK',
                 'question' => 'Motto dari Universitas Surabaya adalah ...',
-                'options' => ['A. Unggul dan Terdepan', 'B. To be the first University in Heart and Mind', 
-                                'C. Bridging Education to the World', 'D. To be the Best University'],
+                'options' => [
+                    'A. Unggul dan Terdepan',
+                    'B. To be the first University in Heart and Mind',
+                    'C. Bridging Education to the World',
+                    'D. To be the Best University'
+                ],
                 'answer' => 'B. To be the first University in Heart and Mind',
                 'is_relic' => false,
                 'answered' => false,
@@ -174,8 +210,12 @@ class RelicController extends Controller
             5 => [
                 'code' => 'HIJAU',
                 'question' => 'Siapakah rektor Universitas Surabaya?',
-                'options' => ['A. Prof. Dr.rer.nat. Maria Goretti Marianti Purwanto', 'B. Djuwari, S.T., Ph.D.', 
-                                'C. Dr. Ir. Benny Lianto, MMBAT.', 'D. Dr. Novianty Kresna Darmasetiawan, S.Psi., M.Si.'],
+                'options' => [
+                    'A. Prof. Dr.rer.nat. Maria Goretti Marianti Purwanto',
+                    'B. Djuwari, S.T., Ph.D.',
+                    'C. Dr. Ir. Benny Lianto, MMBAT.',
+                    'D. Dr. Novianty Kresna Darmasetiawan, S.Psi., M.Si.'
+                ],
                 'answer' => 'C. Dr. Ir. Benny Lianto, MMBAT.',
                 'is_relic' => false,
                 'answered' => false,
@@ -193,8 +233,12 @@ class RelicController extends Controller
             7 => [
                 'code' => 'AKAR',
                 'question' => 'Universitas Surabaya dulunya bernama: ',
-                'options' => ['A. Universitas Indonesia Timur', 'B. Universitas Trisakti', 
-                                'C. Universitas Trisakti Surabaya', 'D. Universitas Nasional Surabaya'],
+                'options' => [
+                    'A. Universitas Indonesia Timur',
+                    'B. Universitas Trisakti',
+                    'C. Universitas Trisakti Surabaya',
+                    'D. Universitas Nasional Surabaya'
+                ],
                 'answer' => 'C. Universitas Trisakti Surabaya',
                 'is_relic' => true,
                 'answered' => false,
@@ -230,8 +274,12 @@ class RelicController extends Controller
             11 => [
                 'code' => 'TARSIUS',
                 'question' => 'Organisasi yang ada di Fakultas Teknik Universitas Surabaya, kecuali ...',
-                'options' => ['A. Badan Eksekutif Mahasiswa Fakultas Teknik', 'B. Dewan Perwakilan Mahasiswa Fakultas Teknik', 
-                                'C. Kelompok Studi Mahasiswa Teknik Mesin dan Manufaktur', 'D. Kelompok Studi Mahasiswa Teknik Sipil'],
+                'options' => [
+                    'A. Badan Eksekutif Mahasiswa Fakultas Teknik',
+                    'B. Dewan Perwakilan Mahasiswa Fakultas Teknik',
+                    'C. Kelompok Studi Mahasiswa Teknik Mesin dan Manufaktur',
+                    'D. Kelompok Studi Mahasiswa Teknik Sipil'
+                ],
                 'answer' => 'D. Kelompok Studi Mahasiswa Teknik Sipil',
                 'is_relic' => true,
                 'answered' => false,
@@ -278,7 +326,7 @@ class RelicController extends Controller
         $kelompok = "A";
         $arr = $_SESSION['kartuA'];
         $_SESSION['player'] = [['kelompokA' => $request->kelompokA, "poin" => 0], ['kelompokB' => $request->kelompokB, "poin" => 0]];
-        $startTime = now()->toIso8601String(); 
+        $startTime = now()->toIso8601String();
         $_SESSION['waktuMulai'] = $startTime;
         $duration = 7 * 60;
         return view('displaycards', compact('kelompok', 'arr', 'startTime', 'duration'));
@@ -313,7 +361,7 @@ class RelicController extends Controller
         } else {
             $arr = $_SESSION['kartuB'];
         }
-        for ($i = 1; $i < count($arr); $i++) {
+        for ($i = 1; $i <= count($arr); $i++) {
             if ($request->kode == $arr[$i]['code']) {
                 $cek = true;
                 $pertanyaan = $arr[$i]['question'];
@@ -371,6 +419,7 @@ class RelicController extends Controller
                 $jawabanBenar = $arr[$i]['answer'];
                 $arr[$i]['answered'] = true;
                 $arr[$i]['image'] = $imgTerjawab[$i];
+                $isrelic = $arr[$i]['is_relic'];
                 break;
             }
         }
@@ -380,30 +429,44 @@ class RelicController extends Controller
         } else {
             $_SESSION['kartuB'] = $arr;
         }
-        
+
         $message = "";
         if ($jawabanUser === $jawabanBenar) {
-            if ($kelompok == "A") {
-                $_SESSION['player'][0]['poin'] += 1;
+            $message = "Jawaban Benar! ";
+
+            if ($isrelic) {
+                if ($kelompok == "A") {
+                    $_SESSION['player'][0]['poin'] += 1;
+                } else if ($kelompok == "B") {
+                    $_SESSION['player'][1]['poin'] += 1;
+                }
+                $message .= "Kartu ini adalah Relic, +1 Poin!";
             } else {
-                $_SESSION['player'][1]['poin'] += 1;
+                $message .= "Maaf kartu ini Zonk!";
             }
-            $message = "Jawaban Benar!";
             return response()->json(['message' => $message]);
         } else {
-            $message = "Jawaban Salah!";
+            $message = "Jawaban Salah! ";
+
+            if($isrelic) {
+                $message .= "Kartu ini adalah Relic!";
+            } else {
+                $message .= "Kartu ini adalah Zonk!";
+            }
+
             return response()->json(['message' => $message]);
         }
     }
 
-    public function endrelic(Request $request) {
+    public function endrelic(Request $request)
+    {
         session_start();
-        if($_SESSION['player'][0]['poin'] > $_SESSION['player'][1]['poin']) {
+        if ($_SESSION['player'][0]['poin'] > $_SESSION['player'][1]['poin']) {
             $pemenang = $_SESSION['player'][0]['kelompokA'];
-            $pesan = $pemenang." menang dengan ".$_SESSION['player'][0]['poin']." poin!";
-        } else if($_SESSION['player'][0]['poin'] < $_SESSION['player'][1]['poin']) {
+            $pesan = $pemenang . " menang dengan " . $_SESSION['player'][0]['poin'] . " poin!";
+        } else if ($_SESSION['player'][0]['poin'] < $_SESSION['player'][1]['poin']) {
             $pemenang = $_SESSION['player'][1]['kelompokB'];
-            $pesan = $pemenang." menang dengan ".$_SESSION['player'][1]['poin']." poin!";
+            $pesan = $pemenang . " menang dengan " . $_SESSION['player'][1]['poin'] . " poin!";
         } else {
             $pesan = "Silahkan mencari 1 kartu fisik tambahan lagi untuk kedua tim!";
         }
